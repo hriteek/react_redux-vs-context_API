f7aa6b28f55f4b3159c1ecc9b067ead335a19fd5
hriteek <hriteekbista@gmail.com>
2020-03-04 15:41:40 +0545

    abstracting the context logic in gobal state

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,8 +1,75 @@
 import React, { Component } from "react";
+import ShopContext from "./shop-context";
 
 class GlobalState extends Component {
+  state = {
+    products: [
+      { id: "p1", title: "Gaming Mouse", price: 29.99 },
+      { id: "p2", title: "Harry Potter 3", price: 9.99 },
+      { id: "p3", title: "Used plastic bottle", price: 0.99 },
+      { id: "p4", title: "Half-dried plant", price: 2.99 }
+    ],
+    cart: [],
+    cartSum: 0
+  };
+  addProductToCart = product => {
+    const updatedCart = [...this.state.cart];
+    const updatedItemIndex = updatedCart.findIndex(
+      item => item.id === product.id
+    );
+
+    if (updatedItemIndex < 0) {
+      updatedCart.push({ ...product, quantity: 1 });
+    } else {
+      const updatedItem = {
+        ...updatedCart[updatedItemIndex]
+      };
+      updatedItem.quantity++;
+      updatedCart[updatedItemIndex] = updatedItem;
+    }
+    this.setState(state => {
+      return {
+        ...state,
+        cart: updatedCart
+      };
+    });
+  };
+  removeProductFromCart = productID => {
+    const updatedCart = [...this.state.cart];
+    const updatedItemIndex = updatedCart.findIndex(
+      item => item.id === productID
+    );
+
+    const updatedItem = {
+      ...updatedCart[updatedItemIndex]
+    };
+    updatedItem.quantity--;
+    if (updatedItem.quantity <= 0) {
+      updatedCart.splice(updatedItemIndex, 1);
+    } else {
+      updatedCart[updatedItemIndex] = updatedItem;
+    }
+    this.setState(state => {
+      return {
+        ...state,
+        cart: updatedCart
+      };
+    });
+  };
+
   render() {
-    return this.props.children;
+    return (
+      <ShopContext.Provider
+        value={{
+          products: this.state.products,
+          cart: this.state.cart,
+          addProductToCart: this.addProductToCart,
+          removeProductFromCart: this.removeProductFromCart
+        }}
+      >
+        {this.props.children}
+      </ShopContext.Provider>
+    );
   }
 }
 
